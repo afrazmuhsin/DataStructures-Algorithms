4fc0a232f4cb635c3ff8ef8e43658a45a03b1688
afrazmuhsin <afrazmuhsin@gmail.com>
2023-11-29 20:02:36 -0600

    added rotation of array using pq

---
+++ src/main/java/org/example/RotateArray.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.*;
+
+public class RotateArray {
+    public ArrayDeque<Integer> rotateArray(Integer[] arr , int k){
+        ArrayDeque<Integer> dq = new ArrayDeque<>(List.of(arr));
+        for(int i=0;i<k;i++){
+            Integer val = dq.removeLast();
+            dq.addFirst(val);
+        }
+        return dq;
+    }
+}
