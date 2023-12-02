6d5c41b3540aa8502c22221e82036daffdee9f1e
afrazmuhsin <afrazmuhsin@gmail.com>
2023-12-01 23:39:50 -0600

    empty check

--- src/test/java/Easy/ConnectingWordsTest.java
+++ src/test/java/Easy/ConnectingWordsTest.java
@@ -4,6 +4,7 @@ import org.example.ConnectingWords;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,5 +33,13 @@ public class ConnectingWordsTest {
         int maxLength = 12;
         List<String> result = cw.connectingWordsTest(inputList,maxLength);
         Assert.assertEquals(result,Arrays.asList("1p3acres123"));
+    }
+
+    @Test
+    void testConnectingWordsTest4(){
+        List<String> inputList = new ArrayList<>();
+        int maxLength = 12;
+        List<String> result = cw.connectingWordsTest(inputList,maxLength);
+        Assert.assertEquals(result,Arrays.asList(""));
     }
 }
