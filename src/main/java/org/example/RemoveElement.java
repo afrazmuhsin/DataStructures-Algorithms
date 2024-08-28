9086a2668c2f005a078c372afdfeded75d6477c6
afrazmuhsin <afrazmuhsin@gmail.com>
2024-08-28 13:53:34 -0500

    Update RemoveElement.java

--- src/main/java/org/example/RemoveElement.java
+++ src/main/java/org/example/RemoveElement.java
@@ -3,7 +3,6 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-//Test Commit 2
 public class RemoveElement {
     public int removeElement(ArrayList<Integer> list, int k){
         for(int i=0;i<list.size();i++){
