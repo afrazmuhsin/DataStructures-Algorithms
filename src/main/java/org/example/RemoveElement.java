f4c1975e44a93437abc1c261a93c5965de008751
afrazmuhsin <afrazmuhsin@gmail.com>
2024-08-28 13:21:48 -0500

    Commit 2
    Commit 2

--- src/main/java/org/example/RemoveElement.java
+++ src/main/java/org/example/RemoveElement.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+//Test Commit 2
 public class RemoveElement {
     public int removeElement(ArrayList<Integer> list, int k){
         for(int i=0;i<list.size();i++){
